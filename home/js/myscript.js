bb1123fff0a00adaed0c42ff2076cdcaca101286
Shandrae Lois Quianzon <saquianzon@student.apc.edu.ph>
2024-12-16 14:38:21 +0800

    added function3

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -2,8 +2,13 @@ function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
   }
 
-  function myFunction2() {
+function myFunction2() {
     const element = document.getElementsByTagName("p");
     document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
-
   }
+
+function myFunction3() {
+  const x = document.getElementsByClassName("intro");
+  document.getElementById("demo").innerHTML = 
+  'The first paragraph (index 0) with class="intro" is: ' + x[0].innerHTML;
+}
