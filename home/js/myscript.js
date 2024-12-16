322d95e1941eb2d4d61ee23f3a6695d8610978ef
Shandrae Lois Quianzon <saquianzon@student.apc.edu.ph>
2024-12-16 14:54:09 +0800

    hehehe

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -4,7 +4,7 @@ function myFunction() {
 
 function myFunction2() {
     const element = document.getElementsByTagName("p");
-    document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
+    document.getElementById("demo").innerHTML = element[0].innerHTML;
   }
 
 function myFunction3() {
