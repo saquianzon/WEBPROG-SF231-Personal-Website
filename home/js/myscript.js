78a1236acbebe5330a8dc204b55740c0736088b2
Shandrae Lois Quianzon <saquianzon@student.apc.edu.ph>
2024-12-16 14:32:29 +0800

    added function2

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -1,3 +1,9 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
   }
+
+  function myFunction2() {
+    const element = document.getElementsByTagName("p");
+    document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
+
+  }
