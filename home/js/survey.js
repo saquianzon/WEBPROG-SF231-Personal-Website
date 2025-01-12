2a168272628c0dd65d15a867d6b17fce3568f4ba
Shandrae Lois Quianzon <saquianzon@student.apc.edu.ph>
2025-01-12 10:35:48 +0800

    adjusted survey and removed its vue code

--- home/js/survey.js
+++ home/js/survey.js
@@ -1,67 +1,99 @@
-const surveyApp = Vue.createApp({
-    data() {
-      return {
-        started: false, // To track if the survey has started
-        currentQuestionIndex: 0,
-        selectedAnswer: null,
-        showThankYou: false,
-        feedback: [],
-        questions: [
-          {
-            question: "Please rate the design of this website",
-            options: ["Yes", "Yes", "Yes", "Yes"],
-          },
-          {
-            question: "Just kidding lol. How would you rate the design of this website?",
-            options: ["Excellent", "Good", "Average", "Poor"],
-          },
-          {
-            question: "Was the website easy to navigate?",
-            options: ["Very Easy", "Somewhat Easy", "Neutral", "Difficult"],
-          },
-          {
-            question: "Would you recommend this website to others?",
-            options: ["Definitely", "Probably", "Not Sure", "No"],
-          },
-          {
-            question: "Any suggestions for improvement?",
-            options: ["Better Design", "More Features", "Faster Loading", "None"],
-          },
-        ],
-      };
+document.addEventListener("DOMContentLoaded", function () {
+  const app = {
+    started: false,
+    currentQuestionIndex: 0,
+    selectedAnswer: null,
+    showThankYou: false,
+    feedback: [],
+    questions: [
+      {
+        question: "Please rate the design of this website",
+        options: ["Yes", "Yes", "Yes", "Yes"],
+      },
+      {
+        question: "Just kidding lol. How would you rate the design of this website?",
+        options: ["Excellent", "Good", "Average", "Poor"],
+      },
+      {
+        question: "Was the website easy to navigate?",
+        options: ["Very Easy", "Somewhat Easy", "Neutral", "Difficult"],
+      },
+      {
+        question: "Would you recommend this website to others?",
+        options: ["Definitely", "Probably", "Not Sure", "No"],
+      },
+      {
+        question: "Any suggestions for improvement?",
+        options: ["Better Design", "More Features", "More Interactions", "None"],
+      },
+    ],
+
+    startSurvey() {
+      this.started = true;
+      this.updateScreen();
     },
-    computed: {
-      currentQuestionData() {
-        return this.questions[this.currentQuestionIndex];
-      },
+
+    selectAnswer(index) {
+      this.selectedAnswer = index;
+      document.getElementById("nextButton").style.display = "inline-block";
+      const options = document.querySelectorAll("#optionsList li");
+      options.forEach((option, i) => {
+        option.classList.toggle("selected", i === index);
+      });
     },
-    methods: {
-      startSurvey() {
-        this.started = true; // Start the survey
-      },
-      selectAnswer(index) {
-        this.selectedAnswer = index;
-      },
-      nextQuestion() {
-        this.feedback.push({
-          question: this.currentQuestionData.question,
-          answer: this.currentQuestionData.options[this.selectedAnswer],
-        });
-        this.selectedAnswer = null;
-        if (this.currentQuestionIndex < this.questions.length - 1) {
-          this.currentQuestionIndex++;
-        } else {
-          this.showThankYou = true;
-          this.started = false;
-        }
-      },
-      restartSurvey() {
-        this.currentQuestionIndex = 0;
-        this.feedback = [];
-        this.selectedAnswer = null;
-        this.showThankYou = false;
+
+    nextQuestion() {
+      this.feedback.push({
+        question: this.questions[this.currentQuestionIndex].question,
+        answer: this.questions[this.currentQuestionIndex].options[this.selectedAnswer],
+      });
+      this.selectedAnswer = null;
+      document.getElementById("nextButton").style.display = "none";
+
+      if (this.currentQuestionIndex < this.questions.length - 1) {
+        this.currentQuestionIndex++;
+      } else {
+        this.showThankYou = true;
         this.started = false;
-      },
+      }
+      this.updateScreen();
     },
-  });
-  surveyApp.mount("#surveyApp");
+
+    restartSurvey() {
+      this.currentQuestionIndex = 0;
+      this.feedback = [];
+      this.selectedAnswer = null;
+      this.showThankYou = false;
+      this.started = false;
+      this.updateScreen();
+    },
+
+    updateScreen() {
+      const startScreen = document.getElementById("startScreen");
+      const questionScreen = document.getElementById("questionScreen");
+      const thankYouScreen = document.getElementById("thankYouScreen");
+      const questionText = document.getElementById("questionText");
+      const optionsList = document.getElementById("optionsList");
+
+      startScreen.style.display = this.started ? "none" : this.showThankYou ? "none" : "block";
+      questionScreen.style.display = this.started ? "block" : "none";
+      thankYouScreen.style.display = this.showThankYou ? "block" : "none";
+
+      if (this.started) {
+        const currentQuestion = this.questions[this.currentQuestionIndex];
+        questionText.textContent = currentQuestion.question;
+        optionsList.innerHTML = "";
+        currentQuestion.options.forEach((option, index) => {
+          const li = document.createElement("li");
+          li.textContent = option;
+          li.addEventListener("click", () => this.selectAnswer(index));
+          optionsList.appendChild(li);
+        });
+      }
+    },
+  };
+
+  document.getElementById("startButton").addEventListener("click", () => app.startSurvey());
+  document.getElementById("nextButton").addEventListener("click", () => app.nextQuestion());
+  document.getElementById("restartButton").addEventListener("click", () => app.restartSurvey());
+});
